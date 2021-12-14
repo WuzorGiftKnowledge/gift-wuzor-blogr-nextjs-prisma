76c2ef29cc4d32633d061fa66bd70fa223add97b
unknown <WuzorGiftKnowledge>
2021-12-14 19:34:22 +0100

    main

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+// pages/api/auth/[...nextauth].ts
+
+import { NextApiHandler } from 'next';
+import NextAuth, { User as NextAuthUser } from 'next-auth';
+import Providers from 'next-auth/providers/github';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+//import prisma from '../../../lib/prisma';
+import { PrismaClient } from '@prisma/client';
+
+
+
+interface NextAuthUserWithStringId extends NextAuthUser {
+  id: string
+}
+const prisma= new PrismaClient();
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    Providers({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        } as NextAuthUserWithStringId
+      },
+    }),
+  ],
+  adapter: PrismaAdapter( prisma ),
+  secret: process.env.SECRET,
+};
