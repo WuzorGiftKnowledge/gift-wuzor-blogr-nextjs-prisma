56a69ce2f6a22155e13549967730f43b92c57234
Gift Wuzor <giftwuzor@Gifts-MBP-2.Home>
2025-12-01 12:12:00 +0000

    added testimony and prayer point functionalities

--- pages/admin/testimony/pending.tsx
+++ pages/admin/testimony/pending.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Layout from '../../../../components/Layout';
+import Layout from '../../../components/Layout';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 
